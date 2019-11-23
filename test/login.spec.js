57c0690a96c614216b3dd6f1f6eef1bc7736799d
Abdul <abz_aniki@yahoo.co.id>
2019-11-23 16:15:34 +0700

    Update for V3

--- test/login.spec.js
+++ test/login.spec.js
@@ -5,19 +5,15 @@ chai.use(chaiHttp);
 
 describe('Test Login Process', async () => {
   let backendPage;
-  let frontendPage;
 
   before(async () => {
     backendPage = await browser.newPage();
-    frontendPage = await browser.newPage();
 
     await backendPage.goto(process.env.BACKEND_URL);
-    await frontendPage.goto(process.env.FRONTEND_URL);
   });
 
   after(async () => {
     await backendPage.close();
-    await frontendPage.close();
   });
 
   it('should work', async () => {
@@ -27,57 +23,17 @@ describe('Test Login Process', async () => {
   });
 
   it('should access BACKEND page', async () => {
-    expect(await backendPage.title()).to.eql('Welcome to Feathers');
+    expect(await backendPage.title()).to.eql('Edubox 3.0');
   });
 
   it('should login via BACKEND', async () => {
-    const expect = global.expect;
-
-    chai
-      .request(process.env.BACKEND_URL)
-      .post('/auth')
-      .send({
-        strategy: 'local',
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD
-      })
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(201);
-
-        expect(res.body).to.have.property('accessToken');
-        expect(res.body).to.have.property('data');
-
-        const token = res.body.accessToken;
-
-        chai
-          .request(process.env.BACKEND_URL)
-          .get('/labs')
-          .set('Authorization', 'Bearer ' + token)
-          .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(200);
-
-            expect(res.body).to.have.property('total');
-            expect(res.body).to.have.property('limit');
-            expect(res.body).to.have.property('skip');
-            expect(res.body).to.have.property('data');
-          });
-      });
-  });
-
-  it('should access FRONTEND page', async () => {
-    expect(await frontendPage.title()).to.eql('Login');
-  });
-
-  it('should login via FRONTEND', async () => {
-    const usernameInput = '#mat-input-0';
-    const passwordInput = '#mat-input-1';
+    const usernameInput = '#LoginForm_username';
+    const passwordInput = '#LoginForm_password';
     const submitSelector = '[type=submit]';
 
-    linkEmail = await frontendPage.$(usernameInput);
-    linkPassword = await frontendPage.$(passwordInput);
-    linkSubmit = await frontendPage.$(submitSelector);
+    linkEmail = await backendPage.$(usernameInput);
+    linkPassword = await backendPage.$(passwordInput);
+    linkSubmit = await backendPage.$(submitSelector);
 
     await linkEmail.click({ clickCount: 2 });
     await linkEmail.type(process.env.USERNAME); // add the email address for linkedin //
@@ -86,11 +42,11 @@ describe('Test Login Process', async () => {
     await linkPassword.type(process.env.PASSWORD); // add password for linkedin account
 
     await linkSubmit.click();
-    await frontendPage.waitForNavigation({ timeout: 5000 });
-    await frontendPage.waitFor(500);
+    await backendPage.waitForNavigation({ timeout: 5000 });
+    await backendPage.waitFor(500);
 
-    await frontendPage.screenshot({ path: 'result.png' });
+    await backendPage.screenshot({ path: 'result.png' });
 
-    expect(frontendPage.url()).to.include('/home');
+    expect(backendPage.url()).to.include('/guru/option/atur');
   });
 });
