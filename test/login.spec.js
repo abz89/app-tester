e5adec6fdc4aa9114766930665f4d59a45fd3b2c
Abdul <abz_aniki@yahoo.co.id>
2019-11-23 20:09:28 +0700

    Update for V4

--- test/login.spec.js
+++ test/login.spec.js
@@ -3,7 +3,7 @@ const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 
-describe('Test Login Process', async () => {
+describe('Test Pairing Process', async () => {
   let backendPage;
   let frontendPage;
 
@@ -27,70 +27,135 @@ describe('Test Login Process', async () => {
   });
 
   it('should access BACKEND page', async () => {
-    expect(await backendPage.title()).to.eql('Welcome to Feathers');
+    expect(await backendPage.title()).to.eql('Error');
   });
+
+  it('should access FRONTEND page', async () => {
+    expect(await frontendPage.title()).to.eql('Edubox 4.0');
+  });
+
+  it('should do PAIRING via FRONTEND', async () => {
+    const pairSelector = '#pswrd ~ button ~ button';
+    const inputSelector = 'input';
+    const buttonSelector = 'button';
+
+    linkPair = await frontendPage.$(pairSelector);
+    linkInputs = await frontendPage.$$(inputSelector);
+    linkButtons = await frontendPage.$$(buttonSelector);
+
+    await linkPair.click({
+      clickCount: 1
+    });
+
+    await linkInputs[0].click({ clickCount: 2 });
+    await linkInputs[0].type(process.env.USERNAME);
+
+    await linkInputs[1].click({ clickCount: 2 });
+    await linkInputs[1].type(process.env.PASSWORD);
+
+    await linkButtons[1].click({
+      clickCount: 1
+    });
+
+    await frontendPage.waitForNavigation({ timeout: 5000 });
+    await frontendPage.waitFor(500);
+
+    await frontendPage.screenshot({ path: 'result-pair.png' });
+
+    expect(frontendPage.url()).to.include('/admin');
+  });
+
+  /* it('should logging out via FRONTEND', async () => {
+    const textSelector = '.text-xs-center';
+    const buttonSelector = 'button';
+
+    linkText = await frontendPage.$$(textSelector);
+    linkButtons = await frontendPage.$$(buttonSelector);
+
+    await linkText[1].click({
+      clickCount: 1
+    });
+
+    await linkButtons[0].click({
+      clickCount: 1
+    });
+
+    await frontendPage.waitForNavigation({ timeout: 5000 });
+    await frontendPage.waitFor(500);
+
+    await frontendPage.screenshot({ path: 'result-logout.png' });
+
+    expect(frontendPage.url()).to.include('/login');
+  });
+
+  it('should LOGIN via FRONTEND', async () => {
+    const usernameInput = '#usrnm';
+    const passwordInput = '#pswrd';
+    const loginSelector = '#pswrd ~ button';
+
+    linkEmail = await frontendPage.$(usernameInput);
+    linkPassword = await frontendPage.$(passwordInput);
+    linkLogin = await frontendPage.$(loginSelector);
+
+    await linkEmail.click({ clickCount: 2 });
+    await linkEmail.type(process.env.USERNAME);
+
+    await linkPassword.click({ clickCount: 2 });
+    await linkPassword.type(process.env.PASSWORD);
+
+    await linkLogin.click({
+      clickCount: 1
+    });
+
+    await frontendPage.waitForNavigation({ timeout: 5000 });
+    await frontendPage.waitFor(500);
+
+    await frontendPage.screenshot({ path: 'result-login.png' });
+
+    expect(frontendPage.url()).to.include('/admin');
+  });
+
+  it('should logging out via FRONTEND', async () => {
+    const textSelector = '.text-xs-center';
+    const buttonSelector = 'button';
+
+    linkText = await frontendPage.$$(textSelector);
+    linkButtons = await frontendPage.$$(buttonSelector);
+
+    await linkText[1].click({
+      clickCount: 1
+    });
+
+    await linkButtons[0].click({
+      clickCount: 1
+    });
+
+    await frontendPage.waitForNavigation({ timeout: 5000 });
+    await frontendPage.waitFor(500);
+
+    await frontendPage.screenshot({ path: 'result-logout.png' });
+
+    expect(frontendPage.url()).to.include('/login');
+  }); */
 
   it('should login via BACKEND', async () => {
     const expect = global.expect;
 
     chai
       .request(process.env.BACKEND_URL)
-      .post('/auth')
+      .post('/auth/login')
       .send({
-        strategy: 'local',
         username: process.env.USERNAME,
         password: process.env.PASSWORD
       })
       .end((err, res) => {
         expect(err).to.be.null;
-        expect(res).to.have.status(201);
+        expect(res).to.have.status(200);
 
-        expect(res.body).to.have.property('accessToken');
-        expect(res.body).to.have.property('data');
-
-        const token = res.body.accessToken;
-
-        chai
-          .request(process.env.BACKEND_URL)
-          .get('/labs')
-          .set('Authorization', 'Bearer ' + token)
-          .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(200);
-
-            expect(res.body).to.have.property('total');
-            expect(res.body).to.have.property('limit');
-            expect(res.body).to.have.property('skip');
-            expect(res.body).to.have.property('data');
-          });
+        // expect(res.body).to.have.property('auth');
+        expect(res.body.auth).to.be.true;
+        expect(res.body).to.have.property('token');
+        expect(res.body).to.have.property('_id');
       });
-  });
-
-  it('should access FRONTEND page', async () => {
-    expect(await frontendPage.title()).to.eql('Login');
-  });
-
-  it('should login via FRONTEND', async () => {
-    const usernameInput = '#mat-input-0';
-    const passwordInput = '#mat-input-1';
-    const submitSelector = '[type=submit]';
-
-    linkEmail = await frontendPage.$(usernameInput);
-    linkPassword = await frontendPage.$(passwordInput);
-    linkSubmit = await frontendPage.$(submitSelector);
-
-    await linkEmail.click({ clickCount: 2 });
-    await linkEmail.type(process.env.USERNAME); // add the email address for linkedin //
-
-    await linkPassword.click({ clickCount: 2 });
-    await linkPassword.type(process.env.PASSWORD); // add password for linkedin account
-
-    await linkSubmit.click();
-    await frontendPage.waitForNavigation({ timeout: 5000 });
-    await frontendPage.waitFor(500);
-
-    await frontendPage.screenshot({ path: 'result.png' });
-
-    expect(frontendPage.url()).to.include('/home');
   });
 });
